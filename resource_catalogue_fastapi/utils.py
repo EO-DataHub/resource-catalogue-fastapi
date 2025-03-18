de2a0a65e85e459afd0f3051792bce4e297d19db
Alex Palmer <alex.palmer@telespazio.com>
2025-03-18 10:05:44 +0000

    test whole endpoint except for execute order

--- resource_catalogue_fastapi/utils.py
+++ resource_catalogue_fastapi/utils.py
@@ -356,7 +356,7 @@ def execute_order_workflow(
     logger.info(f"Sending request to {url} with payload: {payload}")
 
     # TODO: Remove
-    raise NotImplementedError("Sending of request is halted for debugging purposes")
+    return "Sending of request is halted for debugging purposes"
 
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
