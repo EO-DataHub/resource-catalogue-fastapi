f4c3fad928a63e54b664075be8109700202c7fb5
Alex Palmer <alex.palmer@telespazio.com>
2025-05-22 14:53:30 +0100

    Update no api key found error

--- resource_catalogue_fastapi/__init__.py
+++ resource_catalogue_fastapi/__init__.py
@@ -924,9 +924,19 @@ def quote(
     api_key = get_api_key(catalog.value, workspace)
 
     if api_key is None:
+
+        def a_or_an(word: str) -> str:
+            return "An" if word[0].lower() in "aeiou" else "A"
+
         return JSONResponse(
             status_code=403,
-            content={"detail": "You do not have access to quote this item"},
+            content={
+                "detail": (
+                    f"{a_or_an(catalog.value)} {catalog.value} API key could not be found for this "
+                    "workspace. An API key must be linked to obtain quotes for commercial data. "
+                    "Please check that an API key is linked in your workspace settings."
+                )
+            },
         )
 
     if catalog.value == OrderableCatalogue.airbus.value:
